44fa631ffd04aaefa201af7d81e7ee8e31ee60fa
Fredrik Glendrange <frgl@computas.com>
2021-11-15 18:11:31 +0100

    Speedtest Logger

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger {
+    class Program {
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello SpeedTestLogger!");
+
+   var config = new LoggerConfiguration();
+   var runner = new SpeedTestRunner(config.LoggerLocation);
+   var testData = runner.RunSpeedTest();
+   var results = new TestResult
+   {
+       SessionId = Guid.NewGuid(),
+       User = config.UserId,
+       Device = config.LoggerId,
+       Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+       Data = testData
+   };
+}
+    }
+}
